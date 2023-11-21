c162a4ea65998df7eedd271ea254abab51dfb173
Priyanka-Das9621 <priyanka.das@cozentus.com>
2023-11-21 12:08:44 +0530

    added build files

---
+++ app.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path')
+//const nocache = require('nocache');
+
+
+// create express app
+var app = express();
+
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }))
+
+// parse application/json
+app.use(bodyParser.json())
+app.use(express.static(__dirname + '/dist/poseidon-ng'));
+//app.use(nocache());
+app.get('/*', (req, res) => res.sendFile(path.join(__dirname)));
+
+
+
+
+// listen for requests
+app.listen(8080, function(){
+    console.log("Server is listening on port 8080");
+});
