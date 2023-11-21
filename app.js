13257fda12a3af912283daef3260497ea262accd
Priyanka-Das9621 <priyanka.das@cozentus.com>
2023-11-21 12:50:29 +0530

    update app.js

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(bodyParser.urlencoded({ extended: true }))
 
 // parse application/json
 app.use(bodyParser.json())
-app.use(express.static(__dirname + '/dist/poseidon-ng'));
+app.use(express.static(__dirname + '/dist/poseidon-ng/'));
 //app.use(nocache());
 app.get('/*', (req, res) => res.sendFile(path.join(__dirname)));
 
