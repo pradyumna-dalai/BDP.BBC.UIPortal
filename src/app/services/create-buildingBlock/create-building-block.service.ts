1584ba2a32e72f04fcec334d5bcdb2c61b9ae8cd
kalindi-24 <99382433+kalindi-24@users.noreply.github.com>
2023-11-27 21:00:55 +0530

    ExplorerView
    ExplorerView

--- src/app/services/create-buildingBlock/create-building-block.service.ts
+++ src/app/services/create-buildingBlock/create-building-block.service.ts
@@ -1,6 +1,7 @@
 import * as settings from "../../../app/common/lib/api-constants";
 import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -10,7 +11,15 @@ export class CreateBuildingBlockService {
   constructor(protected http: HttpClient) { }
   public createBuildingBlock(body) {
     // return this.http.get<any>(settings.AppRoutes.Auth.createBuildingBlock,body);
-   //  return this.http.post<any>("http://localhost:5000/buildingblocks/api/v1/buildingblock", body);
-   return this.http.post<any>(settings.AppRoutes.Auth.createBuildingBlock,body);
+    //  return this.http.post<any>("http://localhost:5000/buildingblocks/api/v1/buildingblock", body);
+    return this.http.post<any>(settings.AppRoutes.Auth.createBuildingBlock, body);
   }
+
+
+  public getExplorerData() {
+    return this.http.get<any>("http://localhost:5000/buildingblocks/api/v1/buildingblock?status=1");
+    //return this.http.get<any>(settings.AppRoutes.Auth.getexploreViewBuildingBlock);
+
+  }
+
 }
