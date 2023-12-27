c187b2086773b633e5777fb7f2001c7c436fdcdb
Kalindi Tripathi <CZ0101@cztech.com>
2023-12-27 13:10:36 +0530

    Env Changes for Demo
    Env Changes for Demo

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -19,8 +19,8 @@ export const environment = {
   sessionTimeoutInterval: 5 * 60 * 1000,
   sessionDialogInterval: 1 * 60 * 1000,
 
-  // endpoint_url:"http://localhost:5000/buildingblocks/api/v1",
-  endpoint_url:"http://bbc-dev-api.eba-wumjpfkg.us-east-1.elasticbeanstalk.com",
+endpoint_url:"http://54.86.49.91:5000",
+ // endpoint_url:"http://bbc-dev-api.eba-wumjpfkg.us-east-1.elasticbeanstalk.com",
 };
 
 
