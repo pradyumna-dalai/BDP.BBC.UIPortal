1188a93989e4ffabbd351b8a17355779a92f69cf
Kalindi Tripathi <CZ0101@cztech.com>
2024-01-03 19:27:40 +0530

    updated env
    updated env

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -13,6 +13,6 @@ export const environment = {
     
     // endpoint_url:"http://localhost:5000/buildingblocks/api/v1",
  // endpoint_url:"http://bbc-dev-api.eba-wumjpfkg.us-east-1.elasticbeanstalk.com",
-  endpoint_url:"http://54.86.49.91:5000",
+  endpoint_url:"ec2-54-86-49-91.compute-1.amazonaws.com:5000",
   };
   
