a07943bb2709dc54556d1d5d83352a236c5d1093
Kalindi Tripathi <CZ0101@cztech.com>
2024-01-03 20:35:51 +0530

    test

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
  // endpoint_url:"http://localhost:5000/buildingblocks/api/v1",
  //endpoint_url:"http://bbc-dev-api.eba-wumjpfkg.us-east-1.elasticbeanstalk.com",
- endpoint_url:"ec2-54-86-49-91.compute-1.amazonaws.com:5000",
+ endpoint_url:"http://ec2-54-86-49-91.compute-1.amazonaws.com:5000",
 };
