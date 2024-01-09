0420417031c6ad31160c888c567097018d53b614
Priyanka-Das9621 <priyanka.das@cozentus.com>
2024-01-09 12:04:59 +0530

    grid

--- src/app/demo/master-data/charge-code/charge-code.component.ts
+++ src/app/demo/master-data/charge-code/charge-code.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { DatePipe } from '@angular/common';
+import { AppBreadcrumbService } from 'src/app/app.breadcrumb.service';
+import { ConfirmationService, MessageService, SelectItem } from 'primeng/api';
+import { Router } from '@angular/router';
+import { MasterDataService } from 'src/app/services/master-dataserivce/master-data.service';
 
 @Component({
   selector: 'app-charge-code',
@@ -6,5 +11,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./charge-code.component.scss']
 })
 export class ChargeCodeComponent {
+
+  // constructor(private datePipe: DatePipe, private breadcrumbService: AppBreadcrumbService, private messageService: MessageService, private confirmationService: ConfirmationService, private router: Router, private masterDataService: MasterDataService) {
+  //   this.breadcrumbService.setItems([
+  //     { label: 'Location' }
+  //   ]);
+  // }
 
 }
