c432e46d18a3c9d8aad5b4af12fd314114d7df43
Priyanka-Das9621 <priyanka.das@cozentus.com>
2024-01-10 15:49:28 +0530

    updated changes

--- src/app/app.menu.component.ts
+++ src/app/app.menu.component.ts
@@ -10,12 +10,13 @@ import { NavigationEnd, Router } from '@angular/router';
         <ul class="layout-menu">
             <li *ngFor="let item of menuItems" app-menuitem [item]="item" [index]="item.id" [root]="true" [class.active]="isMenuItemActive(item.routerLink)"></li>
         </ul>
-    `,styles: [`
-        .active {
-            background-color: #007bff; /* Apply your active state styling here */
-            color: #fff; /* Text color for active state */
-        }
-    `]
+    `
+    // ,styles: [`
+    //     .active {
+    //         background-color: #007bff; /* Apply your active state styling here */
+    //         color: #fff; /* Text color for active state */
+    //     }
+    // `]
 })
 export class AppMenuComponent implements OnInit {
 
--- src/app/demo/master-data/charge-code/charge-code.component.ts
+++ src/app/demo/master-data/charge-code/charge-code.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { DatePipe } from '@angular/common';
 import { AppBreadcrumbService } from 'src/app/app.breadcrumb.service';
-import { ConfirmationService, MessageService, SelectItem } from 'primeng/api';
+import { ConfirmationService, SelectItem } from 'primeng/api';
 import { Router } from '@angular/router';
 import { MasterDataService } from 'src/app/services/master-dataserivce/master-data.service';
 
@@ -11,15 +11,15 @@ import { MasterDataService } from 'src/app/services/master-dataserivce/master-da
   styleUrls: ['./charge-code.component.scss']
 })
 export class ChargeCodeComponent {
-  // visibleChargeCode:boolean=false;
+  visibleChargeCode:boolean=false;
 
-  // constructor(private datePipe: DatePipe, private breadcrumbService: AppBreadcrumbService, private messageService: MessageService,
-  //    private confirmationService: ConfirmationService, private router: Router, private masterDataService: MasterDataService) {
-  //   this.breadcrumbService.setItems([
-  //     { label: 'Chrage code' }
-  //   ]);
-  // }
-  // showDialogChargeCode(){
-  //   this.visibleChargeCode = true;
-  // }
+  constructor(private datePipe: DatePipe, private breadcrumbService: AppBreadcrumbService,
+     private confirmationService: ConfirmationService, private router: Router, private masterDataService: MasterDataService) {
+    this.breadcrumbService.setItems([
+      { label: 'Chrage code' }
+    ]);
+  }
+  showDialogChargeCode(){
+    this.visibleChargeCode = true;
+  }
 }
