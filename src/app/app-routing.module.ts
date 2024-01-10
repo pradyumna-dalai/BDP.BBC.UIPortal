e43bf4d388d4064934b3eb90ed919386b164b27e
Kalindi Tripathi <CZ0101@cztech.com>
2024-01-10 20:29:46 +0530

    routing for Charge Code
    routing for Charge Code

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,6 +8,7 @@ import { GlossaryComponent } from './demo/view/glossary/glossary.component';
 import { CreateBbComponent } from './demo/view/create-bb/create-bb.component';
 import { CreateProjectComponent } from './demo/view/project/create-project/create-project.component';
 import { LocationsComponent } from './demo/master-data/locations/locations.component';
+import { ChargeCodeComponent } from './demo/master-data/charge-code/charge-code.component';
 
 
 
@@ -41,7 +42,8 @@ import { LocationsComponent } from './demo/master-data/locations/locations.compo
                 path: '', component: AppMainComponent,
                 children: [
                     { path: '', redirectTo: 'building-block', pathMatch: 'full' },
-                    { path: 'locations', component: LocationsComponent }
+                    { path: 'locations', component: LocationsComponent },
+                    { path: 'charge-code', component: ChargeCodeComponent },
                 ]
             },
             { path: 'notfound', component: AppNotfoundComponent },
