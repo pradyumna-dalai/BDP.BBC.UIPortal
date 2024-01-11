7405278bd5ec2ecd9709c385f979866119216d58
Priyanka-Das9621 <priyanka.das@cozentus.com>
2024-01-11 11:30:58 +0530

    chargecode grid commit

--- src/app/demo/master-data/charge-code/charge-code.component.ts
+++ src/app/demo/master-data/charge-code/charge-code.component.ts
@@ -1,25 +1,28 @@
 import { Component } from '@angular/core';
-import { DatePipe } from '@angular/common';
-import { AppBreadcrumbService } from 'src/app/app.breadcrumb.service';
-import { ConfirmationService, SelectItem } from 'primeng/api';
 import { Router } from '@angular/router';
+import { ConfirmationService, MessageService } from 'primeng/api';
+import { AppBreadcrumbService } from 'src/app/app.breadcrumb.service';
 import { MasterDataService } from 'src/app/services/master-dataserivce/master-data.service';
 
 @Component({
   selector: 'app-charge-code',
   templateUrl: './charge-code.component.html',
-  styleUrls: ['./charge-code.component.scss']
+  styleUrls: ['./charge-code.component.scss'],
+  providers: [MessageService, ConfirmationService]
 })
 export class ChargeCodeComponent {
-  visibleChargeCode:boolean=false;
 
-  constructor(private datePipe: DatePipe, private breadcrumbService: AppBreadcrumbService,
-     private confirmationService: ConfirmationService, private router: Router, private masterDataService: MasterDataService) {
+  display: boolean = false;
+  chargeCode: string = '';
+  description: string = '';
+  status: string = '';
+
+  constructor(private breadcrumbService: AppBreadcrumbService, private messageService: MessageService, private confirmationService: ConfirmationService, private router: Router, private masterDataService: MasterDataService) {
     this.breadcrumbService.setItems([
-      { label: 'Chrage code' }
+      { label: 'ChargeCode' }
     ]);
   }
-  showDialogChargeCode(){
-    this.visibleChargeCode = true;
+  showDialog() {
+    this.display = true;
   }
 }
