eeaf42dd0a158dacc506b8e44a8cb9c41700f50e
Priyanka-Das9621 <priyanka.das@cozentus.com>
2024-02-22 15:37:24 +0530

    env url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -21,7 +21,7 @@ export const environment = {
 
   // endpoint_url:"http://localhost:5000/buildingblocks/api/v1",
   //endpoint_url:"http://bbc-dev-api.eba-wumjpfkg.us-east-1.elasticbeanstalk.com",
-  endpoint_url: "http://ec2-34-205-39-55.compute-1.amazonaws.com",
+  endpoint_url: "http://ec2-44-193-79-71.compute-1.amazonaws.com:5000",
 
 
 };
