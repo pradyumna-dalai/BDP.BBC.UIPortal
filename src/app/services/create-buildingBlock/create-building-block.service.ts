4b84cb3646ff9e5fb74aa18abf8da3c8e3e127a6
Kalindi Tripathi <CZ0101@cztech.com>
2024-03-14 12:46:36 +0530

    Revert "Merge branch 'Demo' into NEWBBC_ForUAT"
    This reverts commit 784a5c493785a239358191b86e13326293779202, reversing
    changes made to c3816cf59cc78277d6823c80f1efced27805700c.

--- src/app/services/create-buildingBlock/create-building-block.service.ts
+++ src/app/services/create-buildingBlock/create-building-block.service.ts
@@ -10,6 +10,7 @@ var url = "/buildingblocks/api/v1/"
   providedIn: 'root'
 })
 export class CreateBuildingBlockService {
+
   constructor(protected http: HttpClient) { }
   public saveEditBuildingBlocks(status: number, body: any) {
     const params = new HttpParams().set('status', status.toString());
